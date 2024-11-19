939aa384249b60cc29ecdb1840826d691d2314be
Killian-Aidalinfo <killian.stein@aidalinfo.fr>
2024-11-19 11:47:01 +0100

    correct repos

--- dagger/src/index.ts
+++ dagger/src/index.ts
@@ -1,16 +1,31 @@
 import { dag, Container, Directory, object, func } from "@dagger.io/dagger"
 
 @object()
-class HelloDagger {
+class NodeApiDemo {
+  @func()
+  async publish(source: Directory): Promise<string> {
+    return await this.build(source).publish(
+      "ttl.sh/myapp-" + Math.floor(Math.random() * 10000000)
+    );
+  }
+
   @func()
   build(source: Directory): Container {
-    return dag
-      .container()
-      .from("node:20")
-      .withDirectory("/src", source)
-      .withWorkdir("/src")
-      .withExec(["npm", "install"])
-      .withExec(["npm", "run", "start"])
-      .withExposedPort(8088)
+    const nodeCache = dag.cacheVolume("node")
+    return (
+      dag
+        .container()
+        // start from a base Node.js container
+        .from("node:21-slim")
+        // add the source code at /src
+        .withDirectory("/src", source)
+        // mount the cache volume at /root/.npm
+        .withMountedCache("/root/.npm", nodeCache)
+        // change the working directory to /src
+        .withWorkdir("/src")
+        // run npm install to install dependencies
+        .withExec(["npm", "install"])
+        .withExposedPort(8080)
+    )
   }
 }
